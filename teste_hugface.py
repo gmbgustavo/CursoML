34683e4bd8da75cc5be58f71b7da828b1ddd9b0b
gustavo <gmb.gustavo@gmail.com>
2024-07-02 17:30:08 -0300

    Reformulação

---
+++ teste_hugface.py
@@ -0,0 +1,10 @@
+from transformers import pipeline
+
+modelo = pipeline('fill-mask')
+
+frase = 'I would rather <mask> than losing my life.'
+
+respostas = modelo(frase)
+
+for resposta in respostas:
+    frase = resposta['token_str']
